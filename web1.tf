c9616135d8ccfe5b8b33dc08eb7e52147f43e1c4
Nathan Withers <nathan@thewithz.tech>
2020-10-05 14:35:32 -0400

    Fixes usage of variables

--- web1.tf
+++ web1.tf
@@ -5,13 +5,13 @@ resource "digitalocean_droplet" "web1" {
   size               = "512mb"
   private_networking = true
   ssh_keys = [
-    "${var.ssh_fingerprint}"
+    "var.ssh_fingerprint"
   ]
   connection {
     host        = self.ipv4_address
     user        = "root"
     type        = "ssh"
-    private_key = "${file(var.pvt_key)}"
+    private_key = "file(var.pvt_key)"
     timeout     = "2m"
   }
   provisioner "remote-exec" {
